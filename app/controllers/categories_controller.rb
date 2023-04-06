6cf8355550a278dca2b4ef3945596707344c64a9
Rowan Mackenzie <row.mackenzie@gmail.com>
2023-04-06 10:01:44 -0700

    add categories view and create page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,9 @@
 class CategoriesController < ApplicationController
 
+  def index
+    @category = Category.find(params[:id])
+  end
+
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
